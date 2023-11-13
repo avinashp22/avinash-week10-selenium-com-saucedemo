a85944c4953a58e077958a7de5f6b3491fdb6e58
avinashp22 <avinashp22@gmail.com>
2023-11-13 22:25:26 +0000

    avinash-week10-selenium-com-saucedemo

--- src/ComSaucedemo.java
+++ src/ComSaucedemo.java
@@ -1,3 +1,19 @@
+/**
+ *1. Setup chrome browser.
+ * 2. Open URL.
+ * 3. Print the title of the page.
+ * 4. Print the current url.
+ * 5. Print the page source.
+ * 6. Enter the email to email field.
+ * 7. Enter the password to password field.
+ * 8. Click on Login Button.
+ * 9. Print the current url.
+ * 10. Navigate to baseUrl
+ * 11. Refresh the page.
+ * 12. Close the browser.
+ *
+ */
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
